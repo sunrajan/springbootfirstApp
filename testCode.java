ad7ff74e2709bef851f3919ffb82d9dab0ec2170
sunrajan <49509950+sunrajan@users.noreply.github.com>
2019-04-12 18:08:09 +0530

    Add files via upload

---
+++ testCode.java
@@ -0,0 +1,18 @@
+package sprintBootdemoproject;
+
+
+
+
+public class testCode {
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+		
+		//logger.info("test ");
+		System.out.println(" Hello, SpringBoot on welcome test project own code ");
+		
+
+	}
+
+}
